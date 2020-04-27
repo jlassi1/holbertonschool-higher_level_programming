be4a5224468b4d058b763664a2078eb9643d083a
jlassi1 <khawlajlassi1990@gmail.com>
2020-04-27 15:51:32 +0100

    100-write.py

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 import sys
-stderr = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
-sys.stdout.write(stderr)
+sys.stdout.write('and that piece of art is useful - Dora Korpar, 2015-10-19\n')
 exit(1)
