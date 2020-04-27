f01b21f67c9bbca108f6046b5e4415ee6625421b
jlassi1 <khawlajlassi1990@gmail.com>
2020-04-27 15:28:59 +0100

    check linked list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -17,21 +17,10 @@ listint_t *slow_p = list, *fast_p = list;
 		slow_p = slow_p->next;
 		fast_p = fast_p->next->next;
 		if (slow_p == fast_p)
-		{
-
-			slow_p = list;
-
-			while (slow_p != fast_p)
-			{
-				slow_p = slow_p->next;
-				fast_p = fast_p->next;
-			}
-
 			return (1);
-		}
 
 	}
 
-return (0);
+			return (0);
 
 }
