a038bf78bbf2aea0c4cf688c3a8ebb4ae077e092
jlassi1 <khawlajlassi1990@gmail.com>
2020-05-06 00:02:03 +0100

    palandrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,47 +1,82 @@
 #include "lists.h"
 /**
- *palindrome_check - fun
- *@s: char
- *@i: integer
- *Return: 0 or 1 or result of check
+ * check_palindrome - checks array to see if palindrome
+ * @arr: array of integers
+ * @length: length of palindrome
+ *
+ * Return: 1 if palindrome. 0 if not palindrome.
  */
-int palindrome_check(char *s, int i)
+int check_palindrome(int *arr, int len)
 {
-	if (i <= 1)
-		return (1);
-	else if (*s == s[i - 1])
-		return (palindrome_check(s + 1, i - 2));
-	else
-		return (0);
+	unsigned int i = 0;
+
+	while (arr[i] <= arr[len])
+	{
+		if (arr[i] != arr[len])
+			return (0);
+		i++;
+		len--;
+	}
+	return (1);
+
 }
 /**
- *is_palindrome-checks if a singly linked list is a palindrome.
- *@head: pointer to linked list
- *Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ * palindrome_length - finds length of palindrome
+ * @head: pointer to linked list
+ *
+ * Return: length
+ */
+int palindrome_length(listint_t *head)
+{
+	int len = 0;
+
+	while (head != NULL)
+	{
+		head = head->next;
+		len++;
+	}
+	return (len);
+}
+
+
+/**
+ * is_palindrome - checks a singly linked list to see if palindrome
+ * @head: points to head of list
+ *
+ * Return: 1 if palindrome. Otherwise 0
  */
 int is_palindrome(listint_t **head)
 {
-listint_t *tmp = *head;
-int i = 1, j = 0, x;
-if (head != NULL)
-return (1);
-	while (tmp->next)
+	listint_t *tmp = *head;
+	int *list_arr;
+	unsigned int len = 0;
+	unsigned int i = 0;
+
+	tmp = *head;
+	if (*head == NULL)
+		return (1);
+    while (tmp != NULL)
 	{
+		tmp = tmp->next;
+		len++;
+	}
+
+	if (len == 0)
+		return (1);
+
+	list_arr = malloc(sizeof(int) * len);
+	if (list_arr == NULL)
+		return (0);
+
+	while (tmp != NULL)
+	{
+		list_arr[i] = tmp->n;
 		tmp = tmp->next;
 		i++;
 	}
-	if (i < 1)
-		return (1);
-tmp = *head;
-char *array = malloc(sizeof(int) * i + 1);
-	while (tmp)
-	{
-		array[j] = tmp->n;
-		tmp = tmp->next;
-		j++;
-	}
 
-		x = palindrome_check(array, j);
+	if (check_palindrome(list_arr, len) == 0)
+		return (0);
+	return (1);
 
-return (x);
 }
