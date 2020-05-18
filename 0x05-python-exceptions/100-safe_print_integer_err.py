b6c811925668ab5207819367ef3f90479b3420dc
jlassi1 <khawlajlassi1990@gmail.com>
2020-05-18 21:16:26 +0100

    error message

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,6 +4,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as err:
+    except (TypeError, ValueError) as err:
         stderr.write("Exception: {}\n".format(err))
         return False
