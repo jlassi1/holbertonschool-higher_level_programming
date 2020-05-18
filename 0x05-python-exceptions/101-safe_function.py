4916748cfafa52196cb1f4db563d27621feb1629
jlassi1 <khawlajlassi1990@gmail.com>
2020-05-18 21:32:13 +0100

    101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def safe_function(fct, *args):
-    
+    from sys import stderr 
+    try:
+        res = fct(*args)
+    except Exception as err:
+        print("Exception: {}".format(err), file=stderr)
+        return None
+    else:
+        return res
