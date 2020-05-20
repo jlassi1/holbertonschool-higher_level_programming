e2b81e3cc300a60385cf67139a3751d4ed3d7be9
jlassi1 <khawlajlassi1990@gmail.com>
2020-05-20 21:29:19 +0100

    fixe 'and' & 'or'

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -54,7 +54,7 @@ class Square:
     def position(self, value):
         if type(value) is not tuple:
             raise TypeError('position must be a tuple of 2 positive integers')
-        elif type(value[0]) != int or type(value[1]) != int:
+        elif type(value[0]) != int and type(value[1]) != int:
             raise TypeError('position must be a tuple of 2 positive integers')
         elif value[0] < 0 or value[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
