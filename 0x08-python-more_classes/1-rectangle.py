4c31a98708fa610fab8bbd3b05abdc55202a90c5
jlassi1 <khawlajlassi1990@gmail.com>
2020-05-25 19:51:21 +0100

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Rectangle module."""
+
+
 class Rectangle:
+    """ Defines a Rectangle """
     def __init__(self, width=0, height=0):
         self.__height = height
         self.__width = width
