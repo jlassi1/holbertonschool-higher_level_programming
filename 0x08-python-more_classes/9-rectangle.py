5dd7869d61accce936e70c9d8981968546b037d4
jlassi1 <khawlajlassi1990@gmail.com>
2020-05-25 20:23:52 +0100

    9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -8,8 +8,8 @@ class Rectangle:
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
         type(self).number_of_instances += 1
 
     @property
