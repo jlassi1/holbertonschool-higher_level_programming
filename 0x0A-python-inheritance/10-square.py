c30d68286fa8db3198616a05d5b45849cde380c6
jlassi1 <khawlajlassi1990@gmail.com>
2020-06-02 19:11:29 +0100

    try

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,8 +4,9 @@ Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
+    """ subclass Square"""
     def __init__(self, size):
         """initialization method"""
+        self.integer_validator("size", size)
         self.__size = size
         super().__init__(self.__size, self.__size)
-
