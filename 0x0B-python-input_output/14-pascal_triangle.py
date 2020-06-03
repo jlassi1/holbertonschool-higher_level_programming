75495996d776de1acbe2899bc363fec5c071572f
jlassi1 <khawlajlassi1990@gmail.com>
2020-06-03 23:28:18 +0100

    Pascal's Triangle

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -3,6 +3,15 @@
 
 
 def pascal_triangle(n):
-    matix =[0 for x in n]
-    for i in n:
-        
+    """ Pascal's Triangle """
+    if n <= 0:
+        return []
+
+    T = [[1] * p for p in range(1, 1 + n)]
+    for i in range(n):
+        for k in range(i):
+            if k == 0 or k == i:
+                T[i][k] = 1
+            else:
+                T[i][k] = T[i - 1][k - 1] + T[i - 1][k]
+    return T
