74ef8a1d02cf02b285870185d3d47fc934faff41
jlassi1 <khawlajlassi1990@gmail.com>
2020-06-03 21:45:24 +0100

    13-student.py

--- 0x0B-python-input_output/13-student.py
+++ 0x0B-python-input_output/13-student.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """module"""
 from sys import argv
-save = __import__("7-save_to_json_file").save_to_json_file
-load = __import__("8-load_from_json_file").load_from_json_file
 
 
 class Student:
+    """class Student """
     def __init__(self, first_name, last_name, age):
         """initialization method"""
         self.first_name = first_name
