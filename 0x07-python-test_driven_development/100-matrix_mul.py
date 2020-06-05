b48eed28f0704b505266144d0fbdc8b55deac0de
jlassi1 <khawlajlassi1990@gmail.com>
2020-06-05 11:43:06 +0100

    fixe check 16

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -11,10 +11,12 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_a must be a list")
     if type(m_b) is not list:
         raise TypeError("m_b must be a list")
-    if not isinstance(m_a[i], list):
-        raise TypeError("m_a must be a list of lists")
-    if not isinstance(m_b[i], list):
-        raise TypeError("m_b must be a list of lists")
+    for i in range(len(m_a)):
+        if not isinstance(m_a[i], list):
+            raise TypeError("m_a must be a list of lists")
+    for i in range(len(m_b)):
+        if not isinstance(m_b[i], list):
+            raise TypeError("m_b must be a list of lists")
     if m_a == [] or m_a == [[]]:
         raise ValueError("m_a can't be empty")
     if m_b == [] or m_b == [[]]:
