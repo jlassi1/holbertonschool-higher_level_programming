2eddac56f759dfa9cd08f376c3ead6d1a2281a7d
jlassi1 <khawlajlassi1990@gmail.com>
2020-06-09 16:43:29 +0100

    task18

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -48,9 +48,9 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """function that returns an instance with all attributes already set"""
-        if cls.__name__ == Rectangle:
+        if cls.__name__ == "Rectangle":
             dummy = cls(5, 5)
-        elif cls.__name__ == Square:
+        elif cls.__name__ == "Square":
             dummy = cls(5)
         dummy.update(**dictionary)
         return dummy
