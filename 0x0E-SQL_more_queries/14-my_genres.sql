83193dc6c66684bbbcdc25312cdc381f8942f4dd
jlassi1 <khawlajlassi1990@gmail.com>
2020-07-14 17:39:18 +0100

    14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,8 +2,9 @@
 SELECT tv_genres.name
 FROM tv_genres
 INNER JOIN tv_show_genres
-INNER JOIN tv_shows
 ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_show.id = tv_show_genres.id
 WHERE tv_shows.title = 'Dexter'
 GROUP BY name
 ORDER BY name ASC;
