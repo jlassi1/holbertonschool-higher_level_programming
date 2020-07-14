f2854bcdb44d14479fcb7afec7bade274a18b796
jlassi1 <khawlajlassi1990@gmail.com>
2020-07-14 15:45:19 +0100

    10a

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,4 +3,4 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.genre_id
-ORDER BY title, genre_id ASC;
+ORDER BY title AND genre_id ASC;
