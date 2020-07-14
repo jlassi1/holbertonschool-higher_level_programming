fab7644a64c9977b2a6da5a021b59cbb8a00691e
jlassi1 <khawlajlassi1990@gmail.com>
2020-07-14 13:29:24 +0100

    6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
-    id INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name VARCHAR(256) NOT NULL);
