fae9ed6ee3456af88999c4b64e5b657a1070facd
jlassi1 <khawlajlassi1990@gmail.com>
2020-08-09 18:24:48 +0100

    9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -17,4 +17,6 @@ if __name__ == "__main__":
     for state in session.query(State).order_by(State.id).all():
         if 'a' in state:
             print("{}: {}".format(state.id, state.name))
+        else:
+            pass
     session.close()
