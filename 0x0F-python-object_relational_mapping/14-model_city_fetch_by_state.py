cc779532c22febe9095026cfd62cf8d2d7382736
jlassi1 <khawlajlassi1990@gmail.com>
2020-08-09 22:08:58 +0100

    model_city.py

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module documentation """
+
+from sys import argv
+from sqlalchemy import create_engine
+from model_state import State, Base
+from model_city import City
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    """ main function"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    for st, ct in session.query(State, City).order_by(City.id)
+    .filter(State.id == City.state_id):
+        print("{}: ({}) {}".format(st.name, st.id, ct.name))
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module documentation """
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
+
+Base = declarative_base()
+
+
+class City(Base):
+    """ class documentation"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False,
+                unique=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False)
