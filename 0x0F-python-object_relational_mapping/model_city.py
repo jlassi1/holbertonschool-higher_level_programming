d1cab794360fb05f5f33c5ea29e8422db364b851
jlassi1 <khawlajlassi1990@gmail.com>
2020-08-09 22:33:45 +0100

    ff******

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Module documentation """
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
 
