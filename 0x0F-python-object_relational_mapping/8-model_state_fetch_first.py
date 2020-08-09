131763b9852e76859c3ac1b38a81a4f321ed2d76
jlassi1 <khawlajlassi1990@gmail.com>
2020-08-09 18:05:30 +0100

    8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,7 +14,6 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    for state in session.query(State).order_by(State.id).all():
+    for state in session.query(State).order_by(State.id).one():
         print("{}: {}".format(state.id, state.name))
-        break
     session.close()
