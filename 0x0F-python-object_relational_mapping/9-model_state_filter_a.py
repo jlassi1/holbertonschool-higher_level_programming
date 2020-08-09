0a62ce8d3fba2777d544a519ee529a9f381a7905
jlassi1 <khawlajlassi1990@gmail.com>
2020-08-09 18:34:10 +0100

    9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -14,10 +14,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    for state in session.query(State).order_by(State.id)
-    .filter(State.name.like('%a%')):
-        if 'a' in state:
-            print("{}: {}".format(state.id, state.name))
-        else:
-            pass
+    for state in session.query(State).order_by(State.id).
+    filter(State.name.like('%a%')):
+        print("{}: {}".format(state.id, state.name))
     session.close()
