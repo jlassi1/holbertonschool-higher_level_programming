72deb5b0b8ad3222b6f7faed3883017711bccacf
jlassi1 <khawlajlassi1990@gmail.com>
2020-08-24 13:46:43 +0100

    https://developer.mozilla.org/fr/docs/Web/HTTP/M%C3%A9thode/OPTIONS

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL and displays all HTTP methods the server will accept.
-curl -X OPTIONS $1 -i | grep "Allow:" | cut -d "" -f 1
+curl -X OPTIONS $1 -i | grep "Allow:" | cut -d ""
