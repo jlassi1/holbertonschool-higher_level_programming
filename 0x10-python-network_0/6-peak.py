30c5f590775e244d12061f25beeee6a31a3533e9
jlassi1 <khawlajlassi1990@gmail.com>
2020-08-24 21:52:23 +0100

    task6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,17 +6,17 @@ def find_peak(list_of_integers):
     """ Function find_peak """
     if list_of_integers is None or list_of_integers == []:
         return None
-    for i in range(len(list_of_integers) - 1):
+    for i in range(0, len(list_of_integers)):
         peak = list_of_integers[0]
-        """if i == len(list_of_integers) - 1:
-            #break
-        """
-        if list_of_integers[i] < list_of_integers[i + 1]\
+        if i == len(list_of_integers) - 1:
+            break
+        """if list_of_integers[i] < list_of_integers[i + 1]\
                 and list_of_integers[i] < list_of_integers[i - 1]:
             peak = list_of_integers[i]
-
+            break"""
         if list_of_integers[i] > list_of_integers[i + 1]\
                 and list_of_integers[i] > list_of_integers[i - 1]:
             peak = list_of_integers[i]
+            break
 
     return peak
