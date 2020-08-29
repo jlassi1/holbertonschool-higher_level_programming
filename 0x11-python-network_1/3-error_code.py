3b5ef55910ef2034489a6f0eaf8785796ed946be
jlassi1 <khawlajlassi1990@gmail.com>
2020-08-29 17:54:57 +0100

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""  A script that fetches URL """
+from urllib.error import HTTPError
+from urllib import parse
+from urllib.request import Request, urlopen
+import sys
+
+
+if __name__ == "__main__":
+    req = Request(sys.argv[1])
+    try:
+        response = urlopen(req)
+    except HTTPError as e:
+        print('Error code: ', e.code)
