f1da37a0a821b9bc6b2429355b58e2a9553127f7
jlassi1 <khawlajlassi1990@gmail.com>
2020-08-29 20:00:26 +0100

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""  A script that fetches URL """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    pram = {'email': sys.argv[2]}
+    r = requests.post(sys.argv[1], data=pram)
+    print(r.text)
