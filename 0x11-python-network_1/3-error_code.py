0751346701920dd4614cf831fe2664492412a818
jlassi1 <khawlajlassi1990@gmail.com>
2020-08-29 18:02:01 +0100

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """  A script that fetches URL """
 from urllib.error import HTTPError
-from urllib import parse
 from urllib.request import Request, urlopen
 import sys
 
@@ -10,6 +9,6 @@ if __name__ == "__main__":
     req = Request(sys.argv[1])
     try:
         with urlopen(req) as f:
-            print(f.read().decode())
+            print(f.read().decode('utf-8'))
     except HTTPError as e:
         print('Error code: ', e.code)
