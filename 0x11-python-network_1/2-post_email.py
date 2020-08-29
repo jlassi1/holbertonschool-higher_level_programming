ccda24388cd4a01a4ab53a4a971e400448fc6a45
jlassi1 <khawlajlassi1990@gmail.com>
2020-08-29 17:34:41 +0100

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" """
+import urllib.parse
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+
+    myobj = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(myobj)
+    data = data.encode('ascii')
+
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as f:
+        print(f.read().decode('utf-8'))
