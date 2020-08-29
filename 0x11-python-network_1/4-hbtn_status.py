e0ac4278e58cea02e155498fde5b67104a4bfce8
jlassi1 <khawlajlassi1990@gmail.com>
2020-08-29 18:19:11 +0100

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""  A script that fetches URL """
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.content))
