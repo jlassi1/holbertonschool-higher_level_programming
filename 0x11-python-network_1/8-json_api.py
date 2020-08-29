2177e52f150c0f0f12a77d40184474a7bd6cd636
jlassi1 <khawlajlassi1990@gmail.com>
2020-08-29 21:03:18 +0100

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""  A script that fetches URL """
+import requests
+import sys
+import json
+
+
+if __name__ == "__main__":
+    pram = {'q': ""}
+    if len(sys.argv) == 2:
+        pram['q'] = sys.argv[1]
+
+    r = requests.post('http://0.0.0.0:5000/search_user', data=pram)
+    try:
+        if not r.json():
+            print("No result")
+        else:
+            print("[{}] {}".format(r.json().get('id'), r.json().get('name')))
+    except:
+        print('Not a valid JSON')
