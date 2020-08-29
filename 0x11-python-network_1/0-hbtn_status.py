9581c09fcd68a498d19a32baf2059de81676c989
jlassi1 <khawlajlassi1990@gmail.com>
2020-08-29 11:01:22 +0100

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,9 @@
 import urllib.request
 
 
-the_page = ""
-#req = urllib.request.Request('https://intranet.hbtn.io/status')
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    the_page += "-" + str(response.read())
-
-print(the_page)
+    the_page = response.read()
+    print('Body response:')
+    print("\t- type: {}".format(type(the_page)))
+    print("\t- content: {}".format(the_page))
+    print("\t- utf8 content: {}".format(the_page.decode('utf-8')))
