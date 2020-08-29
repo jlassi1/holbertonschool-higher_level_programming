43194eb1432e48ec95d4b994596f214bab365d79
jlassi1 <khawlajlassi1990@gmail.com>
2020-08-29 18:30:41 +0100

    5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""  A script that fetches URL """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    print(r.headers.get('X-Request-Id'))
