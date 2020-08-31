f035bfa59fc0d07f25c43f488d73af9a49a02e64
jlassi1 <khawlajlassi1990@gmail.com>
2020-08-31 11:23:32 +0100

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """  A script that fetches URL """
 import requests
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
-    r = requests.get('https://api.github.com/user', auth=(sys.argv[1], sys.argv[2]))
+    r = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
     print(r.json().get('id'))
