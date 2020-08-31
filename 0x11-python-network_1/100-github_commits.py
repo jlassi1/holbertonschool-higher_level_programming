06876d4b6a21636822716ecce383bcf9e4d787ea
jlassi1 <khawlajlassi1990@gmail.com>
2020-08-31 12:35:11 +0100

    100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""  Problem solve """
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    r = requests.get(
+        'https://api.github.com/repos/{}/{}/commits'
+        .format(argv[2], argv[1]))
+    data = r.json()
+    for i in range(10):
+        name = data[i].get("commit").get("author").get("name")
+        id = data[i].get("sha")
+        print("{}: {}".format(id, name))
