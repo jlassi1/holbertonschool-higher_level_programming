0d67f62102ea6ea799b9c570653ff99b61709aa7
jlassi1 <khawlajlassi1990@gmail.com>
2020-09-15 22:45:55 +0100

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,18 +1,14 @@
 #!/usr/bin/node
 
-if (process.argv.length === 2 || process.argv.length === 3) {
+if (process.argv.length <= 3) {
   console.log('0');
 } else {
   let i = 2;
   let big = parseInt(process.argv[i]);
-  while (process.argv[i]) {
-    if (big < parseInt(process.argv[i + 1])) { big = parseInt(process.argv[i + 1]); }
-    i++;
-  }
-  i = 2;
   let secondbig = parseInt(process.argv[i]);
   while (process.argv[i]) {
-    if (secondbig < parseInt(process.argv[i + 1] && secondbig !== big)) { secondbig = parseInt(process.argv[i + 1]); }
+      if (secondbig < parseInt(process.argv[i + 1] && secondbig !== big)) { secondbig = parseInt(process.argv[i + 1]); }
+    if (big < parseInt(process.argv[i + 1])) { big = parseInt(process.argv[i + 1]); }
     i++;
   }
   console.log(secondbig);
