21f2d432e420989857ca2b993a9b6ede842ca473
jlassi1 <khawlajlassi1990@gmail.com>
2020-09-15 23:42:05 +0100

    103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,7 +4,9 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = function () {};
+myObject.incr = function () {
+  myObject.value += 1;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
