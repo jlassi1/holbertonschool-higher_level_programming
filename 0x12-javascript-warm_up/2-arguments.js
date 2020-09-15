2fcea7a012abc258457d5ddf2a798da7658679fd
jlassi1 <khawlajlassi1990@gmail.com>
2020-09-15 20:23:12 +0100

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 if (process.argv[2] === undefined) {
   console.log('No argument');
-} else if (process.argv[2] !== undefined) {
+} else if (process.argv.lenth === 2) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
