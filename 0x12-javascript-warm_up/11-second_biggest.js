9e13f8cf246d11c88eb4066fed13cbab3186ee35
jlassi1 <khawlajlassi1990@gmail.com>
2020-09-16 09:41:03 +0100

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
-  console.log('0');
+if (process.argv.length > 3) {
+  const second = process.argv.sort(function (a, b) { return b - a; })[3];
+  console.log(second);
 } else {
-  const second_biggest = process.argv.sort(function (a, b) { return b - a; })[3];
-  console.log(second_biggest);
+  console.log('0');
 }
