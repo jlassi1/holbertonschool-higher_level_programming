63ee73b484ac33a070e9a8a27d02a3b311af233e
jlassi1 <khawlajlassi1990@gmail.com>
2020-09-22 14:27:00 +0100

    https://nodejs.org/en/knowledge/file-system/how-to-read-files-in-nodejs/

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const argv = process.argv;
 fs.readFile(argv[2], 'utf8', (err, data) => {
   if (err) {
-    console.error(err);
+    console.log(err);
     return;
   }
   console.log(data);
