75114c3a76d5200c5e92c23092c4158b809f2a0a
jlassi1 <khawlajlassi1990@gmail.com>
2020-09-22 16:24:25 +0100

    4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+const argv = process.argv;
+let count = 0;
+request(argv[2], function (error, response, body) {
+  if (error) { console.error('error:', error); }
+
+  const results = JSON.parse(body).results;
+  const Antilles = 'https://swapi-api.hbtn.io/api/people/18/';
+  for (const x in results) {
+    for (const char in results[x].characters) {
+      if (results[x].characters[char] === Antilles) {
+        count++;
+      }
+    }
+  }
+  return console.log(count);
+});
